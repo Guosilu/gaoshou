7ef589d8b16da6652108d79be28cc9812ce32f40
liupengcheng <2212999068@qq.com>
2018-09-27 18:01:05 +0800

    activity

--- config/config.js
+++ config/config.js
@@ -4,6 +4,7 @@ const baseUrl = 'https://aa.zdcom.net.cn/';
 const loginUrl = baseUrl + 'gaoshou/api/loginApi.php'; 
 const uploadUrl = baseUrl + 'gaoshou/api/uploadApi.php';
 const publicationUrl = baseUrl + 'gaoshou/api/publicationApi.php';
+const activityUrl = baseUrl + 'gaoshou/api/activityApi.php';
 const coreUrl = baseUrl + "gaoshou/core/";
 const img = baseUrl + "gaoshou/images/";
 
@@ -13,6 +14,7 @@ module.exports = {
   loginUrl: loginUrl,
   uploadUrl: uploadUrl,
   publicationUrl: publicationUrl,
+  activityUrl: activityUrl,
   coreUrl: coreUrl,
   img: img
 }
