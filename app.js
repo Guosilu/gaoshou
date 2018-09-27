1b8433739117a914c47cba90891b566e2f5336cd
Guosilu <13156007572@163.com>
2018-09-27 08:54:47 +0800

    5555

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const config = require('config/config.js');
 App({
   onLaunch: function () {
-    // 展示本地存储能力1111122222233333333444444
+    // 展示本地存储能力11111222222333333334444445555555555555555555
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
