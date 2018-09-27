825002edc27abbeecd5d282cf6f387d8c998c1b7
liupengcheng <2212999068@qq.com>
2018-09-27 10:28:22 +0800

    lpc

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
 
 // 定义常量
 const baseUrl = 'https://aa.zdcom.net.cn/';
-const loginUrl = baseUrl + 'gaoshou/user/loginApi.php';
+const loginUrl = baseUrl + 'gaoshou/api/loginApi.php';
+const uploadUrl = baseUrl + 'gaoshou/api/uploadApi.php';
 const coreUrl = baseUrl + "gaoshou/core/";
 const img = baseUrl + "gaoshou/images/";
 
@@ -9,6 +10,7 @@ const img = baseUrl + "gaoshou/images/";
 // 对外接口
 module.exports = {
   loginUrl: loginUrl,
+  uploadUrl: uploadUrl,
   coreUrl: coreUrl,
   img: img
 }
