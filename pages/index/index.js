48da7aae58572df378016c944c479c9b6bccf48c
liupengcheng <2212999068@qq.com>
2018-09-28 12:10:21 +0800

    1111

--- pages/index/index.js
+++ pages/index/index.js
@@ -123,33 +123,7 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    };
+  getActivityList: function() {
     var that = this;
     wx.request({
       url: 'http://aa.zdcom.net.cn/gaoshou/api/getlistApi.php',
@@ -165,16 +139,13 @@ Page({
         that.setData({
           taglist: res.data
         });
-
       }
     })
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  onLoad: function() {
+    this.getActivityList();
+  },
+  onShow: function() {
+    this.getActivityList();
   }
 })
