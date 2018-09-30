485494b6db1f4f3c2ea392ddc5737728072f69bd
liupengcheng <2212999068@qq.com>
2018-09-30 10:56:14 +0800

    活动页面点赞功能

--- pages/launch/launch.js
+++ pages/launch/launch.js
@@ -1,15 +1,109 @@
-
+const app = getApp();
 const config = require('../../config/config.js');
 Page({
   data: {
-    detail:{
-
-    }
+    like_status: null,
+    detail:{}
+  },
+  is_like: function (id) {
+    let that = this;
+    wx.request({
+      url: config.activityUrl,
+      method: "POST",
+      data: {
+        action: 'is_like',
+        where: {
+          activity_id: id,
+          openId: app.globalData.openId
+        }
+      },
+      success: function (res) {
+        let like_status;
+        if (res.data == 1) {
+          like_status = 1;
+        } else {
+          like_status = 0;
+        }
+        that.setData({
+          like_status: like_status
+        })
+      }
+    });
+  },
+  like: function () {
+    let that = this;
+    let post = {};
+    let id = that.data.detail.id;
+    post['activity_id'] = id;
+    post['openId'] = app.globalData.openId;
+    wx.request({
+      url: config.activityUrl,
+      method: "POST",
+      data: {
+        action: 'like',
+        id: id,
+        post: post
+      },
+      success: function (res) {
+        if (res.data == 1) {
+          that.setData({
+            like_status: 1
+          })
+          wx.showToast({
+            icon: 'none',
+            title: '点赞成功！'
+          });
+        }
+      }
+    });
+  },
+  like_cancel: function () {
+    let that = this;
+    wx.showModal({
+      title: '提示',
+      content: '确定取消点赞吗？',
+      success: function (res) {
+        if (res.confirm) {
+          let where = {};
+          let id = that.data.detail.id;
+          where['activity_id'] = id;
+          where['openId'] = app.globalData.openId;
+          wx.request({
+            url: config.activityUrl,
+            method: "POST",
+            data: {
+              action: 'like_cancel',
+              id: id,
+              where: where
+            },
+            success: function (res) {
+              if (res.data == 1) {
+                that.setData({
+                  like_status: 0
+                })
+                wx.showToast({
+                  icon: 'none',
+                  title: '已取消点赞！'
+                });
+              }
+            }
+          });
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    });
   },
   joinActivity: function () {
     let id = this.data.detail.id;
     wx.navigateTo({
       url: '../participate/participate?id=' + id
+    })
+  },
+  go_Activity_Initiate: function (res) {
+    console.log(res);
+    wx.switchTab({
+      url: '../activity_Initiate/activity_Initiate',
     })
   },
   /**
@@ -35,16 +129,11 @@ Page({
           that.setData({
             detail: res.data
           })
+          that.is_like(res.data.id);
           wx.hideLoading();
         }
       }
     });
-  },
-  go_Activity_Initiate: function (res) {
-    console.log(res);
-    wx.switchTab({
-      url: '../activity_Initiate/activity_Initiate',
-    })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
