a9a8581ccbe56a27b2154849cebd124ad771240f
Deshun <admin@w3h5.com>
2018-10-18 11:37:05 +0800

    add

--- pages/square/add/index.js
+++ pages/square/add/index.js
@@ -9,12 +9,11 @@ Page({
     isLogin: wx.getStorageSync('isLogin'),
     img: config.img,
     //图片上传
-    file: [],
-    file: 'http://tmp/wxe44bd41006bd0fca.o6zAJs96_CHrZR8aqAQ4o18gcRIs.aAZILES0H18i15de4d5d875ef5d0fdb2198fa3dbe1de.jpg',
+    files: [],
     //广告
-    advert:"",
+    advert: "",
   },
-  form_reset: function () {
+  form_reset: function() {
     this.setData({
       activityTypeIndex: 0,
       bdate: "2016-09-01",
@@ -25,7 +24,7 @@ Page({
       file: ''
     });
   },
-  formSubmit: function (e) {
+  formSubmit: function(e) {
     wx.showLoading({
       mask: true,
       title: '提交中...',
@@ -53,7 +52,7 @@ Page({
       this.fileUpload(file, post);
     }
   },
-  fileUpload: function (path, post) {
+  fileUpload: function(path, post) {
     let that = this;
     wx.uploadFile({
       url: config.uploadUrl,
@@ -62,14 +61,14 @@ Page({
       formData: {
         action: 'upload_file'
       },
-      success: function (res) {
+      success: function(res) {
         console.log(res.data);
         post['thumb'] = res.data;
         that.formSubmitDo(post);
       }
     });
   },
-  formSubmitDo: function (post) {
+  formSubmitDo: function(post) {
     let that = this;
     post['openId'] = app.globalData.openId;
     wx.request({
@@ -79,7 +78,7 @@ Page({
         action: 'add',
         post: post
       },
-      success: function (res) {
+      success: function(res) {
         if (res.data > 0) {
           wx.showToast({
             title: '提交成功！',
@@ -95,12 +94,12 @@ Page({
     })
   },
   //删除图片
-  delImg: function (e) {
+  delImg: function(e) {
     this.setData({
       file: ''
     })
   },
-  bindAccountChange: function (e) {
+  bindAccountChange: function(e) {
     console.log('picker account 发生选择改变，携带值为', e.detail.value);
     if (e.detail.value < 1) {
       wx.showToast({
@@ -114,46 +113,43 @@ Page({
     })
   },
   // 图片上传
-  chooseImage: function (e) {
+  chooseImage: function(e) {
     var that = this;
     wx.chooseImage({
-      count: 3,
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-      success: function (res) {
+      success: function(res) {
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
         that.setData({
-          file: res.tempFilePaths[0],
+          files: that.data.files.concat(res.tempFilePaths)
         });
       }
-    });
-
-
+    })
   },
-  previewImage: function (e) {
+  previewImage: function(e) {
     wx.previewImage({
       current: e.currentTarget.id, // 当前显示图片的http链接
-      urls: this.data.file // 需要预览的图片http链接列表
+      urls: this.data.files // 需要预览的图片http链接列表
     })
   },
   // 改变时间
-  bindDateChange: function (e) {
+  bindDateChange: function(e) {
     this.setData({
       bdate: e.detail.value
     })
   },
-  bindTimeChange: function (e) {
+  bindTimeChange: function(e) {
     this.setData({
       btime: e.detail.value
     })
   },
-  bindeDateChange: function (e) {
+  bindeDateChange: function(e) {
     console.log(e);
     this.setData({
       edate: e.detail.value
     })
   },
-  bindeTimeChange: function (e) {
+  bindeTimeChange: function(e) {
     this.setData({
       etime: e.detail.value
     })
@@ -161,8 +157,8 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    if (options.url){
+  onLoad: function(options) {
+    if (options.url) {
       this.setData({
         file: options.url
       })
@@ -173,14 +169,14 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     this.setData({
       isLogin: wx.getStorageSync('isLogin')
     })
@@ -189,14 +185,14 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
     if (wx.getStorageSync('isLogin')) {
       this.form_reset();
     }
@@ -205,21 +201,21 @@ Page({
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
