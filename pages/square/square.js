636c24e522599ef8b7b9924b9e8f0a3ebda3afee
liupengcheng <2212999068@qq.com>
2018-10-23 09:15:15 +0800

    修复旧bug,增添新bug.

--- pages/square/square.js
+++ pages/square/square.js
@@ -11,6 +11,7 @@ Page({
     img: config.img,
     list :[],
     page: 1,
+    pagesize: 5
   },
   addVideo: function () {
     wx.chooseImage({
@@ -30,22 +31,31 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this;
-    that.query('list', config.squareUrl);
+    /*var that = this;
+    that.query('list', config.squareUrl);*/
   },
   /**
    * 查询
    */
-  query: function (action, url) {
+  query: function (action, url, onShow) {
     // console.log(action);return
+    var onShow = onShow || '';
     var that = this;
+    if (onShow == 1) {
+      var page = 1;
+      var pagesize = that.data.pagesize * that.data.page; 
+    } else {
+      var page = that.data.page;
+      var pagesize = that.data.pagesize;
+    }
     wx.request({
       url: url,
       method: "POST",
       dataType: "JSON",
       data: {
         action: action,
-        page: that.data.page
+        page: page,
+        pagesize: pagesize
       },
       success: function (res) {
         let data = JSON.parse(res.data);
@@ -55,9 +65,16 @@ Page({
         console.log(data);
 
         if (res.data != '[]') {
-          that.setData({
-            list: that.data.list.concat(data)
-          })
+          if (onShow == 1) {
+            that.setData({
+              list: data
+            })
+            wx.hideLoading();
+          } else {
+            that.setData({
+              list: that.data.list.concat(data)
+            })
+          }
         } else {
           wx.showToast({
             title: '暂无更多信息',
@@ -95,14 +112,18 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    wx.showToast({
+      title: '正在加载....',
+      icon: "loading"
+    })
+    this.query('list', config.squareUrl, 1);
   },
 
   /**
