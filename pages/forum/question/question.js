e673c00e167466926d109d3fddd75ea378fea04f
Deshun <admin@w3h5.com>
2018-10-24 18:16:04 +0800

    question

---
+++ pages/forum/question/question.js
@@ -0,0 +1,88 @@
+// pages/forum/question/question.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    contShow: false,
+    sendShow: true,
+    inputVal: "",
+  },
+  inputTyping: function (e) {
+    this.setData({
+      sendShow: false,
+      contShow: true
+    })
+  },
+  sendBtn: function (e) {
+    this.setData({
+      contShow: false,
+      sendShow: true,
+      inputVal: "",
+    })
+  },
+  contReply: function (e) {
+    this.setData({
+      contShow: true,
+      sendShow: false,
+      inputVal: "回复",
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
