cd4cae548ddacae73032a290b9e839cf77f2a50e
Guosilu <13156007572@163.com>
2018-11-02 11:04:08 +0800

    论坛部分

--- pages/forum/add/question/question.js
+++ pages/forum/add/question/question.js
@@ -1,4 +1,6 @@
-// pages/forum/add/q/q.js
+const util = require('../../../../config/comment.js');
+const config = util.config;
+const app = util.app;
 Page({
 
   /**
@@ -8,6 +10,31 @@ Page({
 
   },
 
+  formSubmit: function(param){
+    
+    var param = param.detail.value;
+    console.log(param);
+    return;
+    wx.request({
+      url: config.forum,
+      dataType: "json",
+      method: "post",
+      data: {
+        "action": "addQuestion",
+        "param":{
+          title: param.title,
+          introduce: param.introduce,
+          openId: app.globalData.openId,
+          image: image,
+          state: '3'
+        }
+      },
+      success: function (res) {
+        console.log(res);
+      }
+    })
+
+  },
   /**
    * 生命周期函数--监听页面加载
    */
