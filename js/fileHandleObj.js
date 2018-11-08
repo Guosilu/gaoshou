06396cb20865ceeb56fcfbc497d3c7b094cf7b9c
unknown <unknown@example.com>
2018-11-09 00:43:17 +0800

    123

--- js/fileHandleObj.js
+++ js/fileHandleObj.js
@@ -13,7 +13,7 @@ function upload(fileObjList) {
     var that = this;
     var paramObjList = [];
     var fileObjList = this.fileObjList;
-    var parttTmp = /^(http:\/\/tmp\/).+(\.[0-9a-zA-Z]{1,5})$/;
+    var parttTmp = /^([a-zA-Z]{1,10}:\/\/tmp(\/|_)).+(\.[0-9a-zA-Z]{1,5})$/;
     for (let i = 0; i < fileObjList.length; i++) {
       var fpath = fileObjList[i].filePath;
       var cname = fileObjList[i].columnName;
